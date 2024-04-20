08248c4db3f77394cc76a283db51a4e66c12960e
Orex2105 <130343175+Orex2105@users.noreply.github.com>
2024-04-21 00:26:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from pygame import *
 import random
-import math
 from settings import *
 
 window = display.set_mode((width, height))
@@ -9,7 +8,11 @@ mixer.init()
 font.init()
 display.set_caption("Игра")
 font1 = font.Font(None, 30)
-
+font_for_points = font.Font(None, 30)
+point = {
+    'player': 0,
+    'opponent': 0
+}
 
 class GameSprite(sprite.Sprite):
     def __init__(self, pimage, x, y, size, speed):
@@ -53,57 +56,65 @@ class Enemy(GameSprite):
         if self.move == 'down':
             self.rect.y += self.speed
 
+
 class Ball(GameSprite):
     def __init__(self, pimage, x, y, size, speed):
         super().__init__(pimage, x, y, size, speed)
+        self.speed_x = speed
+        self.speed_y = random.randint(-5, 5)
+        self.default_pos = (0.5*width, 0.5*height)
+
+    def update(self):
+        if self.rect.bottom >= height:
+            self.speed_y = random.randint(-5, -1)
+        elif self.rect.top <= 0:
+            self.speed_y = random.randint(1, 5)
+        if sprite.collide_mask(self, player) or sprite.collide_mask(self, enemy):
+            self.speed_x *= -1
+        if self.rect.left <= 0:
+            point['player'] += 1
+            self.rect.x, self.rect.y = self.default_pos
+        if self.rect.right >= width:
+            point['opponent'] += 1
+            self.rect.x, self.rect.y = self.default_pos
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
 
 player = Player('platform.png', 3.5*(width*0.25), height//4, (43, 206), 3)
 enemy = Enemy('platform.png', 0.3*(width*0.25), height//4, (43, 206), 3)
+ball = Ball('ball.png', 0.5*width, 0.5*height, (32, 32), 5)
 
 finish = False
+render_point = False
 
 while True:
     window.fill(white)
-    player.reset()
-    player.update()
-    enemy.reset()
-    enemy.update()
+    ball.reset(); ball.update()
+    player.reset(); player.update()
+    enemy.reset(); enemy.update()
 
     keys = key.get_pressed()
     for e in event.get():
         if e.type == QUIT:
             exit()
+    if keys[K_TAB]:
+        render_point = True
+    else:
+        render_point = False
     
     if not finish:
         pass
     else:
         pass
 
-    fps_text = font1.render(f'fps: {round(clock.get_fps())}', True, (0, 0, 0))
+    fps_text = font1.render(f'{round(clock.get_fps())}', True, (0, 0, 0))
+    point_text_player = font_for_points.render(f'Соперник: {point["opponent"]}', True, (0, 0, 0))
+    point_text_opponent = font_for_points.render(f'Игрок: {point["player"]}', True, (0, 0, 0))
 
-    window.blit(fps_text, (width//2, 15))
+    window.blit(fps_text, (width - fps_text.get_width() - 10, height-fps_text.get_height()))
+    if render_point:
+        window.blit(point_text_player, (200, 10))
+        window.blit(point_text_opponent, (600, 10))
 
     display.update()
     clock.tick(fps)
-
-
-
-
-'''class Button(GameSprite):
-    def __init__(self, pimage, x, y, size, name, speed):
-        super().__init__(pimage, x, y, speed)
-        self.width = size[0]
-        self.height = size[1]
-        self.button_name = name
-        self.image = transform.scale(image.load(pimage), (self.width, self.height))
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.speed = 0
-
-    def update(self):
-        x_mouse, y_mouse = mouse.get_pos()
-        mouse_rect = Rect(x_mouse, y_mouse, 1, 1)
-        if self.rect.colliderect(mouse_rect):
-            if mouse.get_pressed()[0]:
-                return self.button_name'''
